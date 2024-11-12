1765f1873a9855d4fdb2565adc1e72aadc87fc1e
Juan Caceres <Juandc760@gmail.com>
2024-11-12 16:17:52 -0500

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="flex justify-between items-center p-4">
+        <UButton color="primary" to="/">Home</UButton>
+        <div class="flex space-x-2">
+            <UButton v-for="link in links" :key="link.to" :to="link.to" :label="link.label" />
+        </div>
+    </nav>
+</template>
+
+<script setup>
+const links = [
+    { label: 'Templates', to: '/templates' },
+    { label: 'Processes', to: '/processes' },
+    { label: 'Key Information', to: '/key-info' },
+
+]
+
+</script>
+
