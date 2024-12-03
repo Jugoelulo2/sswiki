c7b1a4969366182b1d3babd2589cc2f0f259d6e3
Juan Caceres <Juandc760@gmail.com>
2024-12-03 16:35:03 -0500

    refactor: removed slug code

--- pages/key-info/[slug].vue
+++ pages/key-info/[slug].vue
@@ -1,31 +1,7 @@
 <template>
-    <UContainer>
-        <h1 class="text-center text-2xl font-bold">{{ keyInfo.title }}</h1>
-        <div class="flex justify-center">
-            <div v-html="keyInfo.content" class="prose w-full"></div>
-        </div>
-
-    </UContainer>
- 
-
-    
+    <h1>Key Info</h1>
 </template>
 
 
-
-
-
-<script setup>
-const route = useRoute()
-const slug = route.params.slug
-const pb = usePocketBase()
-const keyInfo = await pb.collection('key_info').getFirstListItem(`slug = "${slug}"`)
-
-
-// Function to get the full image URL
-const getImageUrl = (fileName) => {
-    return `${pb.baseUrl}/api/files/key_info/${keyInfo.id}/${fileName}`
-}
-</script>
 
 
