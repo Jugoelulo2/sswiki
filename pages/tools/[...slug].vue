16baa6a301fe9c39a20913244cab3afda14760c4
Juan Caceres <Juandc760@gmail.com>
2024-12-05 16:40:02 -0500

    feat(tools): add initial Tools page and index with placeholder content
    - Created a new Tools page and index with a header and placeholder text indicating that the content is a work in progress.
    - Ensured consistent styling with a centered layout for better user experience.

---
+++ pages/tools/[...slug].vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-2xl font-bold text-center pt-4" >Tools</h1>
+    <p class="text-center">work in progress, coming soon</p>
+</template>
---
+++ pages/tools/index.vue
@@ -0,0 +1,8 @@
+<template class="text-center"> 
+    <h1 class="text-2xl font-bold">Tools</h1>
+    <p>work in progress, coming soon</p>
+</template>
+
+<script setup>
+
+</script>
