bf1f1b179eb5bd1eb83191e6bc488ac04e537817
Juan Caceres <Juandc760@gmail.com>
2024-12-05 16:38:17 -0500

    remove: delete unused 404 and templates Vue components to clean up the codebase

--- pages/[...slug].vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>Hello</h1>
-    <p>404</p>
-</template>
--- pages/templates.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1 class="text-center text-2xl font-bold">Templates</h1>   
-    <p class="text-center text-sm text-gray-500">Work in Progress, more to come...</p>
-
-</template>
-
-
-
