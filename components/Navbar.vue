98462342a93f8ac73c9ba86ebd61ef4a20354126
Juan Caceres <Juandc760@gmail.com>
2024-12-05 16:38:06 -0500

    refactor(Navbar): update navigation links and enhance styling for dark mode support

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
-    <nav class="flex justify-between items-center p-4">
-        <UButton color="primary" to="/">Home</UButton>
+    <nav class="flex justify-between items-center p-4 bg-gray-100 dark:bg-gray-800">
+        <UButton color="primary" to="/" variant="link" class="dark:bg-gray-800">Home</UButton>
         <div class="flex space-x-2 items-center">
             <DarkModeButton  size="lg" class="mr-2"/>
-            <UButton v-for="link in links" :key="link.to" :to="link.to" :label="link.label" />
+            <UButton v-for="link in links" :key="link.to" :to="link.to" :label="link.label" variant="link" class="dark:bg-gray-800" />
         </div>
     </nav>
 </template>
 
 <script setup>
 const links = [
-    { label: 'Templates', to: '/templates' },
+    { label: 'Tools', to: '/tools' },
     { label: 'Processes', to: '/processes' },
     { label: 'Key Information', to: '/key-info' },
 ]
---
+++ pages/tools/tools.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-center text-2xl font-bold">Tools</h1>   
+    <p class="text-center text-sm text-gray-500">Work in Progress, more to come...</p>
+
+</template>
+
+
+
